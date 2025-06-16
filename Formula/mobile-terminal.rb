c03e4f0c6da56e961c2ff647e6c9e489a167c788
josharsh <harsh.joshi.pth@gmail.com>
2025-06-16 14:42:17 +0530

    Add mobile-terminal formula v0.2.1

---
+++ Formula/mobile-terminal.rb
@@ -0,0 +1,70 @@
+class MobileTerminal < Formula
+  include Language::Python::Virtualenv
+
+  desc "The terminal moves with you - Access your terminal from any mobile device"
+  homepage "https://github.com/josharsh/term-cast"
+  url "https://files.pythonhosted.org/packages/source/m/mobile-terminal/mobile_terminal-0.2.1.tar.gz"
+  sha256 "6cd78fd17d7686ca7c86692de065e61a8d49a1d8706dd81adc5cdb4b7c5e2387"
+  license "MIT"
+  version "0.2.1"
+
+  depends_on "python@3.12"
+
+  resource "aiofiles" do
+    url "https://files.pythonhosted.org/packages/af/41/cfed10bc64d774f497a86e5ede9248e1d062db675504b41c320954d99641/aiofiles-23.2.1.tar.gz"
+    sha256 "84ec2218d8419404abcb9f0c02df3f34c6e0a68ed41072acfb1cef5cbc29051a"
+  end
+
+  resource "aiohttp" do
+    url "https://files.pythonhosted.org/packages/18/93/1f005bbe044471a0444a82cdd7356f5120b9cf94fe2c50c0cdbf28f1258b/aiohttp-3.9.3.tar.gz"
+    sha256 "90842933e5d1ff760fae6caca4b2b3edba53ba8f4b71e95dacf2818a2aca06f7"
+  end
+
+  resource "aiosignal" do
+    url "https://files.pythonhosted.org/packages/ae/67/0952ed97a9793b4958e5736f6d2b346b414a2cd63e82d05940032f45b32f/aiosignal-1.3.1.tar.gz"
+    sha256 "54cd96e15e1649b75d6c87526a6ff0b6c1b0dd3459f43d9ca11d48c339b68cfc"
+  end
+
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/e3/fc/f800d51204003fa8ae392c4e8278f256206e7a919b708eef054f5f4b650d/attrs-23.2.0.tar.gz"
+    sha256 "935dc3b529c262f6cf76e50877d35a4bd3c1de194fd41f47a2b7ae8f19971f30"
+  end
+
+  resource "frozenlist" do
+    url "https://files.pythonhosted.org/packages/cf/3d/2102257e7acad73efc4a0c306ad3953f68c504c16982bbdfee3ad75d8085/frozenlist-1.4.1.tar.gz"
+    sha256 "c037a86e8513059a2613aaba4d817bb90b9d9b6b69aace3ce9c877e8c8ed402b"
+  end
+
+  resource "multidict" do
+    url "https://files.pythonhosted.org/packages/4a/15/bd620f7a6eb9aa5112c4ef93e7031bcd071e0611763d8e17706ef8ba65e0/multidict-6.0.4.tar.gz"
+    sha256 "3666906492efb76453c0e7b97f2cf459b0682e7402c0489a95484965dbc1da49"
+  end
+
+  resource "yarl" do
+    url "https://files.pythonhosted.org/packages/e0/ad/bedcdccbcbf91363fd425a948994f3340924145c2bc8ccb296f4a1e52c28/yarl-1.9.4.tar.gz"
+    sha256 "566db86717cf8080b99b58b083b773a908ae40f06681e87e589a976faf8246bf"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  def caveats
+    <<~EOS
+      To use mobile-terminal, run:
+        mobile-terminal
+      or the short version:
+        mterm
+
+      To broadcast a specific command:
+        mterm npm install
+        
+      The terminal will show you a URL to open on your phone.
+      Make sure both devices are on the same WiFi network.
+    EOS
+  end
+
+  test do
+    assert_match "The terminal moves with you", shell_output("#{bin}/mobile-terminal --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap for josharsh's tools
+
+  ## Installation
+
+  ```bash
+  brew tap josharsh/tap
+  brew install mobile-terminal
+  ```
